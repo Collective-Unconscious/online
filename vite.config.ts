564b87a946d92c0422b970a3b023eea7319f10d1
roxwize <rae@roxwize.xyz>
2026-06-16 10:18:36 -0400

    fix preprocessor
    Signed-off-by: roxwize <rae@roxwize.xyz>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from "@sveltejs/adapter-node";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		sveltekit({
-			adapter: adapter()
+			adapter: adapter(),
+			preprocess: vitePreprocess()
 		})
 	]
 });
