04135a9a4af83ac208bcd3b029ee7c0fb5375478
areit <77842470+areitpog@users.noreply.github.com>
2026-06-28 01:09:32 +0300

    Small header refactor (#6)
    * layout refinements
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * rm gap
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * layout adjustments
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * fix height issuUAARUARRRGHH
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * feat: clean up the header + mobile view for icon
    
    * refactor: stuff is now in its scss file yay!
    
    * format: remove spaces before />
    
    ---------
    
    Signed-off-by: roxwize <rae@roxwize.xyz>

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ProfileButtons from "./ProfileButtons.svelte";
+	import SiteLogo from "./SiteLogo.svelte";
+</script>
+
+<header id="site-header">
+	<SiteLogo/>
+	<ProfileButtons/>
+</header>
