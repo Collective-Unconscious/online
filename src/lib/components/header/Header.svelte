bb1b26388f113db455f0c7554498a137f8b81847
areit <77842470+areitpog@users.noreply.github.com>
2026-06-28 02:51:55 +0300

    fix forgor to import the style (#9)
    * layout refinements
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * rm gap
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * layout adjustments
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * fix height issuUAARUARRRGHH
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    
    * fix: FUCK
    
    ---------
    
    Signed-off-by: roxwize <rae@roxwize.xyz>
    Co-authored-by: roxwize <rae@roxwize.xyz>

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,6 +1,9 @@
 <script>
 	import ProfileButtons from "./ProfileButtons.svelte";
 	import SiteLogo from "./SiteLogo.svelte";
+
+	// styling
+	import "./style.scss";
 </script>
 
 <header id="site-header">
